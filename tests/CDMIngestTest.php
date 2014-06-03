48f41280685b4838e28033a24ad34da4dbcd1665
Stefan Khan-Kernahan <skhanker@gmail.com>
2014-06-03 02:23:56 -0700

    added remaining tests for array keys

--- tests/CDMIngestTest.php
+++ tests/CDMIngestTest.php
@@ -104,15 +104,93 @@ class CDMIngestTest extends PHPUnit_Framework_TestCase
 
 
 
-
     /**
      * @dataProvider modelProvider
      */
     public function testReservedID(array $m)
     {
-        $this->assertArrayHasKey('_____-id',$m);
+        $this->assertArrayHasKey('--id',$m);
     }
 
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testReservedType(array $m)
+	{
+		$this->assertArrayHasKey('_type',$m);
+	}
+
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testReservedCompund(array $m)
+	{
+		$this->assertArrayHasKey('_compound',$m);
+	}
+
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testReservedCreated(array $m)
+	{
+		$this->assertArrayHasKey('_created',$m);
+	}
+
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testReservedUpdated(array $m)
+	{
+		$this->assertArrayHasKey('_updated',$m);
+	}
+
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testReservedThumb(array $m)
+	{
+		$this->assertArrayHasKey('_thumb',$m);
+	}
+
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testReservedTheme(array $m)
+	{
+		$this->assertArrayHasKey('_theme',$m);
+	}
+
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testReservedMediaHandler(array $m)
+	{
+		$this->assertArrayHasKey('_mediaHandler',$m);
+	}
+
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testSourceResource(array $m)
+	{
+		$this->assertArrayHasKey('sourceResource',$m);
+	}
+
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testWebResource(array $m)
+	{
+		$this->assertArrayHasKey('webResource',$m);
+	}
+
+	/**
+	 * @dataProvider modelProvider
+	 */
+	public function testOriginalRecord(array $m)
+	{
+		$this->assertArrayHasKey('originalRecord',$m);
+	}
 
 
     public function modelProvider()
