c66a93e39cce4d0fd2a1b7180961f335247f4c5f
skhanker <skhanker@gmail.com>
2014-06-03 11:50:27 -0700

    setUpBeforeClass must be static :( changed $model in CDMIngestTest to be static

--- tests/CDMIngestTest.php
+++ tests/CDMIngestTest.php
@@ -8,13 +8,13 @@
         /**
          * @var $model CDMIngest
          */
-        private $model;
+        private static $model;
 
-		public function setUpBeforeClass()
+		public static function setUpBeforeClass()
 		{
 			fwrite(STDOUT, __METHOD__ . "\n");
 			fwrite(STDOUT, "Creating Model\n");
-            $this->model = new CDMIngest('{    "_id": "DL UID",    "_type": "DL TYPE: ITEM",    "_compound": "IS COMPOUND OBJ? (t/f)",    "_created": "DL INGEST TIMESTAMP",    "_updated": "DL UPDATED TIMESTAMP",    "_thumb": "THUMBNAIL URL",    "_theme": "THEME",    "_mediaHandler": "MEDIA HANDLER",    "sourceResource": {        "@type": "edm:ProvidedCHO",        "sourceID": "ID (original)",        "title": {            "eng": "TITLE",            "jap": "JAPANESE TITLE"        },        "creator": "CREATOR",        "contributor": "CONTRIBUTOR",        "publisher": "PUBLISHER",        "description": "DESCRIPTION",        "subject": [            {                "name": "SUBJECT NAME"            },            {                "name": "SUBJECT NAME"            }        ],        "extent": "EXTENT",        "collection": "SOURCE COLLECTION",        "format": "FORMAT",        "type": "TYPE",        "genre": "GENRE",        "temporal": {            "date": "DATE",            "sort": "SORT",            "decade": "DECADE"        },        "source": {            "@id": "URI?",            "name": "SOURCE NAME"        },        "rights": "SOURCE RIGHTS",        "geo": {            "name": "GEO NAME",            "latLong": "LAT/LONG"        }    },    "webResource": {        "@type": "edm:WebResource",        "@id": "WEBRESOURCE ID",        "format": "FORMAT",        "rights": "WEB OBJECT RIGHTS",        "type": "DCMITYPE",        "collection": "COLLECTION",        "hasPart": [            "PAGE 1 URL",            "PAGE 2 URL",            "PAGE etc. URL"        ]    },    "originalRecord": "ORIGINAL RECORD"}');
+            self::$model = new CDMIngest('{    "_id": "DL UID",    "_type": "DL TYPE: ITEM",    "_compound": "IS COMPOUND OBJ? (t/f)",    "_created": "DL INGEST TIMESTAMP",    "_updated": "DL UPDATED TIMESTAMP",    "_thumb": "THUMBNAIL URL",    "_theme": "THEME",    "_mediaHandler": "MEDIA HANDLER",    "sourceResource": {        "@type": "edm:ProvidedCHO",        "sourceID": "ID (original)",        "title": {            "eng": "TITLE",            "jap": "JAPANESE TITLE"        },        "creator": "CREATOR",        "contributor": "CONTRIBUTOR",        "publisher": "PUBLISHER",        "description": "DESCRIPTION",        "subject": [            {                "name": "SUBJECT NAME"            },            {                "name": "SUBJECT NAME"            }        ],        "extent": "EXTENT",        "collection": "SOURCE COLLECTION",        "format": "FORMAT",        "type": "TYPE",        "genre": "GENRE",        "temporal": {            "date": "DATE",            "sort": "SORT",            "decade": "DECADE"        },        "source": {            "@id": "URI?",            "name": "SOURCE NAME"        },        "rights": "SOURCE RIGHTS",        "geo": {            "name": "GEO NAME",            "latLong": "LAT/LONG"        }    },    "webResource": {        "@type": "edm:WebResource",        "@id": "WEBRESOURCE ID",        "format": "FORMAT",        "rights": "WEB OBJECT RIGHTS",        "type": "DCMITYPE",        "collection": "COLLECTION",        "hasPart": [            "PAGE 1 URL",            "PAGE 2 URL",            "PAGE etc. URL"        ]    },    "originalRecord": "ORIGINAL RECORD"}');
 		}
 
 		protected function setUp()
@@ -172,7 +172,7 @@
 		public function modelProvider()
 		{
 			$arr = array();
-			$mod = $this->model->getModelAsArray();
+			$mod = self::$model->getModelAsArray();
 			foreach ($mod as $k => $v){
 				$arr[$k] = $v;
 			}
