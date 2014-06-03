4f2f6a61d82e9ff896767abba494efaf6743c76a
Stefan Khan-Kernahan <skhanker@gmail.com>
2014-06-03 02:26:42 -0700

    added remaining tests for array keys

--- tests/CDMIngestTest.php
+++ tests/CDMIngestTest.php
@@ -1,8 +1,8 @@
 <?php
-class CDMIngestTest extends PHPUnit_Framework_TestCase
-{
+	class CDMIngestTest extends PHPUnit_Framework_TestCase
+	{
 
-    protected $model = '{
+		protected $model = '{
     "_id": "DL UID",
     "_type": "DL TYPE: ITEM",
     "_compound": "IS COMPOUND OBJ? (t/f)",
@@ -66,143 +66,143 @@ class CDMIngestTest extends PHPUnit_Framework_TestCase
     "originalRecord": "ORIGINAL RECORD"
 }';
 
-    public static function setUpBeforeClass()
-    {
-        fwrite(STDOUT, __METHOD__ . "\n");
-    }
+		public static function setUpBeforeClass()
+		{
+			fwrite(STDOUT, __METHOD__ . "\n");
+		}
 
-    protected function setUp()
-    {
-       // fwrite(STDOUT, "\n\n" . __METHOD__ . "\n");
-    }
+		protected function setUp()
+		{
+			// fwrite(STDOUT, "\n\n" . __METHOD__ . "\n");
+		}
 
-    protected function assertPreConditions()
-    {
-        //fwrite(STDOUT, __METHOD__ . "\n");
-    }
+		protected function assertPreConditions()
+		{
+			//fwrite(STDOUT, __METHOD__ . "\n");
+		}
 
-    protected function assertPostConditions()
-    {
-        //fwrite(STDOUT, __METHOD__ . "\n");
-    }
+		protected function assertPostConditions()
+		{
+			//fwrite(STDOUT, __METHOD__ . "\n");
+		}
 
-    protected function tearDown()
-    {
-        //fwrite(STDOUT, __METHOD__ . "\n");
-    }
+		protected function tearDown()
+		{
+			//fwrite(STDOUT, __METHOD__ . "\n");
+		}
 
-    public static function tearDownAfterClass()
-    {
-        fwrite(STDOUT, "\n" . __METHOD__ . "\n");
-    }
+		public static function tearDownAfterClass()
+		{
+			fwrite(STDOUT, "\n" . __METHOD__ . "\n");
+		}
 
-    protected function onNotSuccessfulTest(Exception $e)
-    {
-        //fwrite(STDOUT, "\n" . __METHOD__ . "\n");
-        //throw $e;
-    }
+		protected function onNotSuccessfulTest(Exception $e)
+		{
+			//fwrite(STDOUT, "\n" . __METHOD__ . "\n");
+			throw $e;
+		}
 
 
 
-    /**
-     * @dataProvider modelProvider
-     */
-    public function testReservedID(array $m)
-    {
-        $this->assertArrayHasKey('--id',$m);
-    }
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testReservedID(array $m)
+		{
+			$this->assertArrayHasKey('--id',$m);
+		}
 
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testReservedType(array $m)
-	{
-		$this->assertArrayHasKey('_type',$m);
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testReservedType(array $m)
+		{
+			$this->assertArrayHasKey('_type',$m);
+		}
+
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testReservedCompund(array $m)
+		{
+			$this->assertArrayHasKey('_compound',$m);
+		}
+
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testReservedCreated(array $m)
+		{
+			$this->assertArrayHasKey('_created',$m);
+		}
+
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testReservedUpdated(array $m)
+		{
+			$this->assertArrayHasKey('_updated',$m);
+		}
+
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testReservedThumb(array $m)
+		{
+			$this->assertArrayHasKey('_thumb',$m);
+		}
+
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testReservedTheme(array $m)
+		{
+			$this->assertArrayHasKey('_theme',$m);
+		}
+
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testReservedMediaHandler(array $m)
+		{
+			$this->assertArrayHasKey('_mediaHandler',$m);
+		}
+
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testSourceResource(array $m)
+		{
+			$this->assertArrayHasKey('sourceResource',$m);
+		}
+
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testWebResource(array $m)
+		{
+			$this->assertArrayHasKey('webResource',$m);
+		}
+
+		/**
+		 * @dataProvider modelProvider
+		 */
+		public function testOriginalRecord(array $m)
+		{
+			$this->assertArrayHasKey('originalRecord',$m);
+		}
+
+
+		public function modelProvider()
+		{
+			$arr = array();
+			$json = json_decode($this->model,true);
+			foreach ($json as $k => $v){
+				$arr[$k] = $v;
+			}
+			return array(
+				array($arr)
+			);
+		}
 	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testReservedCompund(array $m)
-	{
-		$this->assertArrayHasKey('_compound',$m);
-	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testReservedCreated(array $m)
-	{
-		$this->assertArrayHasKey('_created',$m);
-	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testReservedUpdated(array $m)
-	{
-		$this->assertArrayHasKey('_updated',$m);
-	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testReservedThumb(array $m)
-	{
-		$this->assertArrayHasKey('_thumb',$m);
-	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testReservedTheme(array $m)
-	{
-		$this->assertArrayHasKey('_theme',$m);
-	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testReservedMediaHandler(array $m)
-	{
-		$this->assertArrayHasKey('_mediaHandler',$m);
-	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testSourceResource(array $m)
-	{
-		$this->assertArrayHasKey('sourceResource',$m);
-	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testWebResource(array $m)
-	{
-		$this->assertArrayHasKey('webResource',$m);
-	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testOriginalRecord(array $m)
-	{
-		$this->assertArrayHasKey('originalRecord',$m);
-	}
-
-
-    public function modelProvider()
-    {
-        $arr = array();
-	$json = json_decode($this->model,true);
-	foreach ($json as $k => $v){
-		$arr[$k] = $v;
-	}
-        return array(
-		array($arr)
-	);
-    }
-}
 ?>
