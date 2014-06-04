2f17104d8bd83312201cf0df1fe1c185b1a9b07c
Stefan Khan-Kernahan <skhanker@gmail.com>
2014-06-03 23:34:18 -0700

    playing around with map

--- www/index.php
+++ www/index.php
@@ -14,23 +14,24 @@
 		echo "var w = {$image_info[0]};\n";
 		echo "var h = {$image_info[1]};\n";
 	 ?>
-	document.getElementById("map").style.width = w/4 + "px";
-	document.getElementById("map").style.height = h/4 + "px";
+	document.getElementById("map").style.width = w/3 + "px";
+	document.getElementById("map").style.height = h/3 + "px";
 
 	var map = L.map('map',{
 		crs: L.CRS.Simple
-	}).setView([50, 100], 3);
+	}).setView([0, 0], 1);
 
 	var southWest = map.unproject([0, w], map.getMaxZoom());
 	var northEast = map.unproject([h, 0], map.getMaxZoom());
 
-	//map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
+	map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
 
 	L.tileLayer('tiles/{z}/{x}/{y}.png', {
 		minZoom: 1,
 		maxZoom: 4,
 		attribution: 'The University of British Columbia',
-		tms: true
+		tms: true,
+		continuousWorld: false
 	}).addTo(map);
 
 </script>
