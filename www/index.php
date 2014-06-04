bf018824dd180cd6dd61772d4e36963e44b6bebd
Stefan Khan-Kernahan <skhanker@gmail.com>
2014-06-03 22:47:04 -0700

    redid tile index

--- www/index.php
+++ www/index.php
@@ -9,6 +9,11 @@
 <script src="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js"></script>
 
 <script>
+	<?
+		$image_info = getimagesize("/var/www/html/dev-driad/www/tiles/map.jpg");
+		echo "var w = {$image_info[0]}";
+		echo "var h = {$image_info[1]}";
+	 ?>
 	var map = L.map('map').setView([0, 0], 2);
 	L.tileLayer('tiles/{z}/{x}/{y}.png', {
 		minZoom: 1,
