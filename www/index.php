6ebf9273b2ab0b423af9f323c55bb9b858454d32
Stefan Khan-Kernahan <skhanker@gmail.com>
2014-06-03 23:05:24 -0700

    playing around with map

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
 	      href="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css" />
 </head>
 <body>
-<div id="map" style="width: 1440px; height: 960px"></div>
+<div id="map" style="width: 960px; height: 600px"></div>
 
 <script src="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js"></script>
 
@@ -14,14 +14,15 @@
 		echo "var w = {$image_info[0]};\n";
 		echo "var h = {$image_info[1]};\n";
 	 ?>
-	document.getElementById("map").style.width = w + "px";
-	document.getElementById("map").style.height = h + "px";
+	document.getElementById("map").style.width = w/4 + "px";
+	document.getElementById("map").style.height = h/2 + "px";
 	var map = L.map('map').setView([0, 0], 2);
 	L.tileLayer('tiles/{z}/{x}/{y}.png', {
 		minZoom: 1,
 		maxZoom: 4,
 		attribution: 'The University of British Columbia',
-		tms: true
+		tms: true,
+		continuousWorld: false
 	}).addTo(map);
 </script>
 </body>
