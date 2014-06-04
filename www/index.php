1a5f3b70786110ca02b11bb351c0b9163b560c3a
Stefan Khan-Kernahan <skhanker@gmail.com>
2014-06-03 23:55:35 -0700

    playing around with map

--- www/index.php
+++ www/index.php
@@ -14,12 +14,12 @@
 		echo "var w = {$image_info[0]};\n";
 		echo "var h = {$image_info[1]};\n";
 	 ?>
-	document.getElementById("map").style.width = w/3 + "px";
-	document.getElementById("map").style.height = h/3 + "px";
+	document.getElementById("map").style.width = "100%";
+	document.getElementById("map").style.height = "100%";
 
 	var map = L.map('map',{
 		crs: L.CRS.Simple
-	}).setView([0, 0], 1);
+	}).setView([-90, -180], 1);
 
 	var southWest = map.unproject([0, w], map.getMaxZoom());
 	var northEast = map.unproject([h, 0], map.getMaxZoom());
