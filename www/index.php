3549e36ca37d00a22635db529856ca702bbb3c77
Stefan Khan-Kernahan <skhanker@gmail.com>
2014-06-05 01:01:17 -0700

    play around with map

--- www/index.php
+++ www/index.php
@@ -56,12 +56,12 @@
 	console.log(actualY(map.getMaxZoom(),bounds._southWest.lng));
 
 	// create an orange rectangle
-	L.rectangle([[bounds._southWest.lat,actualY(map.getMaxZoom(),bounds._southWest.lng)],[bounds._northEast.lat,actualY(map.getMaxZoom(),bounds._northEast.lng)]], {color: "#ff7800", weight: 1}).addTo(map);
+	L.rectangle([[actualY(map.getMaxZoom(),bounds._southWest.lat),bounds._southWest.lng],[actualY(map.getMaxZoom(),bounds._northEast.lat),bounds._northEast.lng]], {color: "#ff7800", weight: 1}).addTo(map);
 	L.rectangle([southWest, northEast], {color: "#ff7800", weight: 1}).addTo(map);
 
 	function actualY (z, y) {
-		//return y - Math.pow(2, (z-2));
-		return Math.pow(3, (z - 2)) + y;
+		return y - Math.pow(2, (z-2));
+		//return Math.pow(2, (z - 2)) + y;
 	}
 </script>
 </body>
